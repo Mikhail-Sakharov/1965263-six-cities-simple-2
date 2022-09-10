440c07e40bae386fcd96480507f08bbe025f5c9d
Mikhail-Sakharov <sakharov_mikhail@hotmail.com>
2022-09-10 20:45:08 +0300

    Реализовал приложение с CLI интерфейсом

---
+++ src/types/offer.ts
@@ -0,0 +1,36 @@
+type Location = {
+  latitude: number;
+  longitude: number;
+};
+
+type City= {
+  name: string;
+  location: Location;
+};
+
+type Host = {
+  name: string;
+  email: string;
+  avatarUrl: string;
+  password: string;
+  isPro: boolean;
+};
+
+export type Offer = {
+    title: string;
+    description: string;
+    date: string;
+    city: City;
+    previewImage: string;
+    images: string[];
+    isPremium: boolean;
+    rating: number;
+    type: string;
+    bedrooms: number;
+    maxAdults: number;
+    price: number;
+    goods: string[];
+    host: Host;
+    commentsCount: number;
+    location: Location;
+  };
