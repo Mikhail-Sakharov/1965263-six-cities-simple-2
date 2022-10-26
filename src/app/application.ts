c16b85f0d2f75427acac6e6bb73feb1fe8128616
Mikhail_Linux <sakharov_mikhail@hotmail.com>
2022-10-26 20:41:46 +0300

    Установил пакет cors и подключил его в express

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,23 +1,16 @@
 import 'reflect-metadata';
 import {inject, injectable} from 'inversify';
+import express, {Express} from 'express';
+import cors from 'cors';
 import {LoggerInterface} from '../common/logger/logger.interface.js';
 import {ConfigInterface} from '../common/config/config.interface.js';
 import {Component} from '../types/component.types.js';
 import {getURI} from '../utils/db.js';
 import {DatabaseInterface} from '../common/database-client/database.interface.js';
-import express, {Express} from 'express';
 import {ControllerInterface} from '../common/controller/controller.interface.js';
 import {ExceptionFilterInterface} from '../common/errors/exception-filter.interface.js';
 import {AuthenticateMiddleware} from '../common/middlewares/authenticate.middleware.js';
 import {getFullServerPath} from '../utils/common.js';
-
-// импорты для тестов
-//import { UserServiceInterface } from '../modules/user/user-service.interface.js';
-//import { OfferServiceInterface } from '../modules/offer/offer-service.interface.js';
-//import { CommentServiceInterface } from '../modules/comment/comment-service.interface.js';
-//import CreateCommentDto from '../modules/comment/dto/create-comment.dto.js';
-//import UpdateOfferDto from '../modules/offer/dto/update-offer.dto.js';
-
 
 @injectable()
 export default class Application {
@@ -30,10 +23,7 @@ export default class Application {
     @inject(Component.CommentController) private commentController: ControllerInterface,
     @inject(Component.OfferController) private offerController: ControllerInterface,
     @inject(Component.UserController) private userController: ControllerInterface,
-    @inject(Component.ExceptionFilterInterface) private exceptionFilter: ExceptionFilterInterface,
-    //@inject(Component.UserServiceInterface) private userService: UserServiceInterface,
-    //@inject(Component.OfferServiceInterface) private offerService: OfferServiceInterface,
-    //@inject(Component.CommentServiceInterface) private commentService: CommentServiceInterface
+    @inject(Component.ExceptionFilterInterface) private exceptionFilter: ExceptionFilterInterface
   ) {
     this.expressApp = express();
   }
@@ -57,6 +47,7 @@ export default class Application {
 
     const authenticateMiddleware = new AuthenticateMiddleware(this.config.get('JWT_SECRET'));
     this.expressApp.use(authenticateMiddleware.execute.bind(authenticateMiddleware));
+    this.expressApp.use(cors());
   }
 
   public initExceptionFilters() {
@@ -82,64 +73,5 @@ export default class Application {
     this.initExceptionFilters();
     this.expressApp.listen(this.config.get('PORT'));
     this.logger.info(`Server started on ${getFullServerPath(this.config.get('HOST'), this.config.get('PORT'))}`);
-
-    // тестирование работы сервисов
-
-    //=======================================================OFFERS====================================================================
-
-    // поиск по id
-    //const offer = await this.offerService.findById('634017bb711b20efa888a078');
-    //console.log(offer);
-
-    // получение всех офферов из БД
-    //const offer = await this.offerService.find();
-    //console.log(offer);
-
-    // обновление оффера
-    /* const exampleOffer = {
-      title: 'Hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh',
-      description: 'Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-      previewImage: 'https://8.react.pages.academy/static/hotel/11.jpg',
-      isPremium: false
-    } as UpdateOfferDto;
-    const offer = await this.offerService.findByIdAndUpdate('63483e58f5ba6ca3253dedc6', exampleOffer);
-    console.log(offer); */
-
-    // удаление оффера
-    //const offer = await this.offerService.findByIdAndDelete('634017bb711b20efa888a075');
-    //console.log(offer);
-
-    // инкремент поля commentsCount
-    //const offer = await this.offerService.incCommentsCount('634017bb711b20efa888a0e4');
-    //console.log(offer);
-
-    //=======================================================COMMENTS==================================================================
-
-    // получение комментариев определённого оффера
-    //const comments = await this.commentService.findByOfferId('634017bb711b20efa888a072');
-    //console.log(comments);
-
-    // добавление комментария
-    /* const exampleComment = {
-      commentText: 'This comment is for changing rating value',
-      commentRating: 2,
-      hostId: '634017ba711b20efa888a055',
-      offerId: '634017bb711b20efa888a078'
-    } as CreateCommentDto;
-    const comment = await this.commentService.create(exampleComment);
-    console.log(comment); */
-
-    //=======================================================USERS====================================================================
-
-    // создание юзера
-    /* const exampleUser = {
-      name: 'Mikhail',
-      email: 'qwe@qweasd.com',
-      avatarUrl: './img/qwe.png',
-      password: '123456',
-      isPro: true
-    };
-    const user = await this.userService.create(exampleUser, 'i74fhwf6t7643gf');
-    console.log(user); */
   }
 }
